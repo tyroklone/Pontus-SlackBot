b7dc64e53994284bdfd6b3e080c5ebd96c265130
Itunuoluwao <46650010+Itunuoluwao@users.noreply.github.com>
2019-09-25 10:20:33 +0100

    Update index.php

--- index.php
+++ index.php
@@ -75,6 +75,10 @@
 
 </body>
 
+<script
+        src="https://code.jquery.com/jquery-3.4.1.min.js"
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+        crossorigin="anonymous"></script>
 <script src="assets/bootstrap.min.js"></script>
 
 </html>
