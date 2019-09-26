ee62cc994b326f59205da5a2dc663cf9452f0146
Itunuoluwao <46650010+Itunuoluwao@users.noreply.github.com>
2019-09-26 20:18:18 +0100

    Update signin.php

--- signin.php
+++ signin.php
@@ -96,7 +96,6 @@
 
                     <div class="form-group">
                             <input id="password" name="password" class="form-control" type="password" placeholder="Password here"/>
-                            <input id="password" class="form-control" type="password" placeholder="Password here"/>
                     </div>
 
                     <div class="form-group">
@@ -106,7 +105,6 @@
 
                     <div class="form-group">
                         <button type="submit" name="login"  class="btn btn-primary btn-block">Sign In</button>
-                        <button type="submit"  class="btn btn-primary btn-block">Sign In</button>
                     </div>
 
                     <hr class="styled-hr">
