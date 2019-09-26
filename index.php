e40ed82687027c3a569983bccd7e268708261f36
Itunuoluwao <46650010+Itunuoluwao@users.noreply.github.com>
2019-09-26 22:26:21 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,10 @@
     <title>HNG: SlackBot</title>
 
     <!-- Styles -->
-    <link href="assets/bootstrap.min.css" rel="stylesheet">
-    <link href="assets/style.css" rel="stylesheet">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">    <link href="assets/style.css" rel="stylesheet">
 
 </head>
 
@@ -74,6 +76,6 @@
 
 </body>
 
-<script src="assets/bootstrap.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
 </html>
