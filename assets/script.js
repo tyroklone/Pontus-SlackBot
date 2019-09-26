89b29c46fa63c74c4dcfb2c28bc789aaec92dfab
Itunuoluwao <46650010+Itunuoluwao@users.noreply.github.com>
2019-09-26 17:39:17 +0100

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,28 @@
 function validateForm(){
+    let fullname = $('#fullname').val();
+    let username = $('#username').val();
+    let email = $('#email').val();
     let username = $('#username').val();
     let password = $('#password').val();
 
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
+    fullname = $.trim(fullname); 
+    username = $.trim(username);
+    email = $.trim(email);
+    password = $.trim(password);
+
+    if (fullname == '') {
+        $('#fullname').parent().addClass('hasError')   
+        $('.alert').html('Fullname cannot be empty').show();
+        return false;
+    }else if(username == ''){
+        $('#username').parent().addClass('hasError')   
+        $('.alert').html('Username cannot be empty').show();
+        return false;
+    }else if(!email.match(mailformat)){
+       $('#email').parent().addClass('hasError')
+       $('.alert').html('Email address is not valid').show();
     
      username = $.trim(username);
      password = $.trim(password);
